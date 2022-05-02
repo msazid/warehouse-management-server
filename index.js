7f2247253703bf927b0df7d80544ff53e52618ee
MD Sazid <mdsaziduli14@gmail.com>
2022-05-02 15:44:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+        res.send('Running Chaccu')
+})
+app.listen(port,()=>{
+    console.log("Sunchi bondhu",port)
+})
+
