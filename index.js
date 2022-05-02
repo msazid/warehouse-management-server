8564a3469ad9eabae9cea85847cf294d60fdc2ee
MD Sazid <mdsaziduli14@gmail.com>
2022-05-02 22:28:33 +0600

    read operation done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const port = process.env.PORT || 5000;
 const cors = require("cors");
@@ -7,11 +8,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ophoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run (){
+        try{
+            await client.connect();
+            const itemCollection = client.db("ms-grocery").collection('item');
+            app.get('/item',async(req,res)=>{
+                const query = {}
+                const cursor = itemCollection.find(query);
+                const items = await cursor.toArray()
+                res.send(items)
+            })
+        }
+        finally{}
+}
+run().catch(console.dir)
+
+
 app.get('/',(req,res)=>{
         res.send('Running Chaccu')
 })
 app.listen(port,()=>{
-    console.log("Sunchi bondhu 50000000000",port)
-    console.log("Sunchi bondhu 50000000000")
+    console.log("Sunchi bondhu 50000000000",port);
 })
 
